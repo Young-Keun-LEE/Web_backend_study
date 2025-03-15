05a290d0885f5ef497ae08d0dd9b8c5e96693521
LEE YOUNGKEUN <153352400+Young-Keun-LEE@users.noreply.github.com>
2025-03-15 09:41:10 +0900

    Delete firstproject directory
    .

--- firstproject/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO article(id, title, content) VALUES(1, '가가가가', '1111');
-INSERT INTO article(id, title, content) VALUES(2, '나나나나', '2222');
-INSERT INTO article(id, title, content) VALUES(3, '다다다다', '3333');
-
-INSERT INTO member(id, email, password) VALUES(1,'justdanny0@naver.com', '1111');
-INSERT INTO member(id, email, password) VALUES(2,'abcMart@gmail.com', '2222');
-INSERT INTO member(id, email, password) VALUES(3,'Mike1342@daum.net', '3333');
