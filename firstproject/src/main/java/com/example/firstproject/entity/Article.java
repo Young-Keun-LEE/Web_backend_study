fe8daadc49f81c03eb5f96e52fcc2d335f6fcb31
LEE YOUNGKEUN <153352400+Young-Keun-LEE@users.noreply.github.com>
2025-03-16 22:55:06 +0900

    Add files via upload
    .

---
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,35 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+
+@AllArgsConstructor
+@ToString
+@Entity
+@Getter
+public class Article {
+    Article() { //Default constructor
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
+
+    public void patch(Article article) {
+        if(article.title != null){
+            this.title = article.title;
+        }
+        if(article.content != null){
+            this.content = article.content;
+        }
+
+    }
+}
